d5c734fa0a727b504c36be3949adf2b35ea2af10
Martin Harrysson <MartinHarrysson1990@hotmail.com>
2024-09-16 16:21:43 +0200

    added more tests

--- tests/controller/teamController.test.ts
+++ tests/controller/teamController.test.ts
@@ -1,7 +1,8 @@
 import request from 'supertest';
 import express from 'express';
 import { getPlayers, getTeams } from '../../src/controller/teamController';
-import { getAllTeams } from '../../src/services/teamService';
+import { getAllTeams, getPlayersByTeamId } from '../../src/services/teamService';
+import { isValidUUID } from '../../src/utils/validate';
 
 
 const server = express();
@@ -21,10 +22,43 @@ describe('team controller integration tests', () => {
         const mockTeams = [{id: "mock id", name: "mock team", logo: "mock url"}];
         (getAllTeams as jest.Mock).mockResolvedValue(mockTeams);
 
-        const response = await request(server).get('/teams');
+        const response = await request(server).get("/teams");
 
         expect(response.status).toBe(200);
         expect(response.body).toEqual(mockTeams);
+    });
+
+    test("Get /players/:teamId should return players for valid team ID", async () =>{
+        const teamId = "valid id";
+        const mockPlayers = [{ id: "mock id", name: "mock Player" }];
+        (isValidUUID as jest.Mock).mockReturnValue(true);
+        (getPlayersByTeamId as jest.Mock).mockResolvedValue(mockPlayers);
+
+        const response = await request(server).get(`/teams/players/${teamId}`);
+
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual(mockPlayers);
+    });
+
+    test("Get /players/:teamId should return error 400 when invalid team id", async ()=> {
+        const teamId = "not valid id";
+        (isValidUUID as jest.Mock).mockReturnValue(false);
+
+        const response = await request(server).get(`/teams/players/${teamId}`);
+
+        expect(response.status).toBe(400);
+        expect(response.text).toBe('Invalid UUID format');
+    });
+
+    test("Get /players/:teamId should return error 500 when server error", async () => {
+        const teamId = "valid id";
+        (isValidUUID as jest.Mock).mockReturnValue(true);
+        (getPlayersByTeamId as jest.Mock).mockRejectedValue(new Error ("Server Error"));
+
+        const response = await request(server).get(`/teams/players/${teamId}`);
+
+        expect(response.status).toBe(500);
+        expect(response.text).toBe("Internal Server Error");
     });
 });
 
