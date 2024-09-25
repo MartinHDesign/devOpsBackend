1a59548bb96f8fdb62e2b15ce4b3f41d3e980bde
Martin Harrysson <MartinHarrysson1990@hotmail.com>
2024-09-25 14:23:57 +0200

    testar med nytt workflow

--- src/index.ts
+++ src/index.ts
@@ -34,6 +34,7 @@ AppDataSource.initialize()
 
         server.listen(port, () => {
             console.log("server is running");
+            console.log("Martin pushar denna uppdatering 09-25 kl 14:23")
             
         })
         
